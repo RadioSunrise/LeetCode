22f2aa5eb3b61c3f715d4b4611742813d8e92182
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-05-14 14:17:52 +0800

    Create twoSum.cpp

---
+++ twoSum.cpp
@@ -0,0 +1,19 @@
+# twoSum
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> MyMap;
+        for(int i = 0; i < nums.size(); i++)
+        {
+            int diff = target - nums[i];
+            if(MyMap.find(diff) != MyMap.end())
+            {
+                return {MyMap[diff], i};
+            }
+            MyMap[nums[i]] = i;
+        }
+        return {};
+    }
+};
+# 用map结构来存放已经遍历过的数和索引，用map/unordered的find来进行查找，可以减少一层循环
+# map结构的用法和map的思想
