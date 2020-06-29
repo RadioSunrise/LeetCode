4f1af7da04c678b53a94de702276e626bb5966be
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-06-29 20:58:06 +0800

    Update and rename reverse-linked-list,java to reverse-linked-list.java

--- Solution/reverse-linked-list.java
+++ Solution/reverse-linked-list.java
@@ -6,10 +6,16 @@
 当part2已经翻转完成的时候，head的next指针依然是指向head.next，那么把head.next的next指针指向head就完成了翻转
 还需要一个p节点来返回翻转完的新头结点, p在第一次找到原链的尾部之后就不再变化了
 */
-public ListNode reverseList(ListNode head) {
-    if (head == null || head.next == null) return head;
-    ListNode p = reverseList(head.next);
-    head.next.next = head;
-    head.next = null;
-    return p;
+class Solution {
+    public ListNode reverseList(ListNode head)
+    {
+        if(head == null || head.next == null)
+        {
+            return head;
+        }
+        ListNode p = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return p;
+    }
 }
