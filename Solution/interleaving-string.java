af7fca036a8cc6647e32c7a6e3da85f2d0db2d4c
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-07-18 14:15:38 +0800

    Update interleaving-string.java

--- Solution/interleaving-string.java
+++ Solution/interleaving-string.java
@@ -48,3 +48,44 @@ class Solution {
         return dp[len1][len2];
     }
 }
+
+
+// dfs+记忆化
+// dfs的思路是，s3的每个字符都有两种选择，可以选s1的，也可以选s2
+// 令i,j,k三个指针分别指向s1，s2和s3，(k的位置有k=i+j)当s3[k] = s1[i]的，那么尝试递归(i+1,j,k+1)
+// 如果不为true，再将i和k回溯，判断j和k，s3[k] = s2[j]，那么尝试(i,j+1,k+1)
+// 对于重复的子问题，可以用记忆化memo来解决，有这个(i,j)对了，就返回结果
+// memo定义为Boolean类，方便判断null的情况
+class Solution {
+    Boolean[][] memo;
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int len1 = s1.length();
+        int len2 = s2.length();
+        int len3 = s3.length();
+        // 特判
+        if ((len1 + len2) != len3) {
+            return false;
+        }
+        memo = new Boolean[len1 + 1][len2 + 1];
+        return dfs(s1,s2,s3,0,0,0);
+    }
+    public Boolean dfs(String s1, String s2, String s3, int i, int j, int k){
+        if(!(memo[i][j] == null)){
+            return memo[i][j];
+        }
+        // 如果k已经到达末尾，返回true
+        if(k == s3.length()){
+            memo[i][j] = true;
+            return true;
+        }
+        boolean valid = false;
+        if(i < s1.length() && s3.charAt(k) == s1.charAt(i)){
+            valid = dfs(s1, s2, s3, i+1, j, k+1);
+        }
+        if(j < s2.length() && s3.charAt(k) == s2.charAt(j)){
+            valid = valid || dfs(s1, s2, s3, i, j+1, k+1);
+        }
+        memo[i][j] = valid;
+        return valid;
+    }
+}
