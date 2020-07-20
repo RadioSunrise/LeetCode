134b8fc02253afbfe2dfe46b34d115a373405a00
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-07-20 15:19:27 +0800

    Create container-with-most-water.java

---
+++ Solution/container-with-most-water.java
@@ -0,0 +1,34 @@
+// site: https://leetcode-cn.com/problems/container-with-most-water/
+
+// 和Two Sum II一样，使用双指针的移动来缩减搜索的空间
+
+class Solution {
+    public int maxArea(int[] height) {
+        // 双指针缩减搜索空间
+        int len = height.length;
+        int left = 0;
+        int right = len - 1;
+        int maxVal = 0;
+        while(left < right) {
+            int capacity = (right - left) * Math.min(height[left], height[right]);
+            if (maxVal < capacity) {
+                maxVal = capacity;
+            }
+            // 当其中一边的高度比较低，高度高的一边向高度低的靠近
+            // 如height[left] < height[right]
+            // 当right向left靠近，高度不会增加，宽度则会减少，因此装水的容量必定减少
+            // 因此left和其他柱子（位于left和right之间）的组合都必定会比[left,right]的组合小
+            // 所以可以排除这一系列的left，则left++
+            if(height[left] < height[right]){
+                left++;
+            }
+            // 同理，如果height[right] < height[left]
+            // 则left向right靠近必定会使容量减少
+            // 这一系列的right组合都可以排除
+            else{
+                right--;
+            }
+        }
+        return maxVal;
+    }
+}
