9795fac603e3269150472692ddaab7589221a2ee
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-09-05 11:10:01 +0800

    Update GetPermutation.java

--- Solution/GetPermutation.java
+++ Solution/GetPermutation.java
@@ -8,8 +8,42 @@ import edu.princeton.cs.algs4.In;
 import java.util.ArrayList;
 
 
-
-
+// 第二版，用LinkedList代替ArrayList，不需要递归，在主函数循环就可以了
+// k先减1，转换成下标，可以方便后面的计算
+// 不需要通过mod来求下一趟的位置，可以用减法来代替mod，相当于缩减数量，主要计算k不要算错
+public String getPermutation(int n, int k) {
+        int[] factor = factorial(n);
+        StringBuilder res = new StringBuilder();
+        LinkedList<Integer> nums = new LinkedList<>();
+        // LinkedList存放有序可变数组
+        for(int i = 1; i <= n; i++){
+            nums.add(i);
+        }
+        // k先减1，转成下标，方便后续计算
+        k--;
+        int index = 0;
+        // i表示剩余长度，开始的时候剩余n个
+        for(int i = n; i > 0; i--){
+            // 计算要放入排列的是剩余数组中的第几个数
+            index = k / factor[i - 1];
+            // 删除并添加到res中
+            res.append(nums.remove(index));
+            // k要缩减 index * factor[i - 1]的数量
+            k -= index * factor[i - 1];
+        }
+        return res.toString();
+    }
+    /**
+     * 返回阶乘数组, n = [0:9]
+     */
+    public int[] factorial(int n){
+        int[] factor = new int[n + 1];
+        factor[0] = 1;
+        for(int i = 1; i < n + 1; i++){
+            factor[i] = i * factor[i - 1];
+        }
+        return factor;
+    }
 
 // 第一版用递归的方法，用ArrayList来存放可变的数组，删除获取比较复杂，k的判断也容易出错
 package leetcode;
