838be0f51e78cb58d6c7a65c79576a3ad14d9a52
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-10-11 18:01:22 +0800

    Update PartitionEqualSubsetSum.java

--- Solution/PartitionEqualSubsetSum.java
+++ Solution/PartitionEqualSubsetSum.java
@@ -54,9 +54,63 @@ public class PartitionEqualSubsetSum {
         }
         return dp[n-1][target];
     }
+
+    /**
+     * 一维dp表的实现，二维降到一维只用一行，因为原先二维的dp过程是根据左上和上决定的
+     * 所以在一维dp表中，要倒序更新，用顺序更新的话每次的修改会改变后续值的原本参考
+     * @param nums
+     * @return
+     */
+    public boolean canPartitionCompress(int[] nums) {
+        int n = nums.length;
+        if(n <= 1){
+            return false;
+        }
+        int sum = 0;
+        int max = nums[0];
+        for (int num : nums){
+            sum += num;
+            max = Math.max(num, max);
+        }
+        // 排除两种情况，总和为奇数或者最大的数比sum/2要大
+        if((sum % 2 == 1) || max > sum / 2){
+            return false;
+        }
+        int target = sum / 2;
+        // 动态规划
+        // 压缩之后剩一维dp[j]
+        boolean[] dp = new boolean[target + 1];
+
+        // 转移方程
+        // (1) nums[i] <= j
+        // 第i个数可以选或者不选
+        // 1.不选：当前i-1个数可以构成j的时候就可以 -> dp[i-1][j]=true => dp[i][j] = true
+        // 2.选：当前i-1个数可以构成j - nums[i]就可以-> dp[i-1][j-nums[i]]=true => dp[i][j] = true
+        // (2) nums[i] > j
+        // 当nums[i] > j，nums[i]是肯定不能选的，所以dp[i][j]等于dp[i-1][j]
+
+        // base case
+        // dp[i][0] = true，都不选就可以；dp[0][nums[0]] = true，其余等于false
+        dp[0] = true;
+        // dp loop
+        for(int i = 1; i < n; i++){
+            int num = nums[i];
+            // 当j小于nums[i]的时候，不用更新的，所以for的终止条件到j >= num就可以
+            for (int j = target; j >= num; j--){
+                if(nums[i] <= j){
+                    dp[j] = dp[j] | dp[j - nums[i]];
+                }
+                // 否则不用更新
+                /*else {
+                    dp[j] = dp[j];
+                }*/
+            }
+        }
+        return dp[target];
+    }
     public static void main(String[] args){
         int[] nums = new int[]{1,5,11,5,2,2};
         PartitionEqualSubsetSum solution = new PartitionEqualSubsetSum();
-        System.out.println(solution.canPartition(nums));
+        System.out.println(solution.canPartitionCompress(nums));
     }
 }
