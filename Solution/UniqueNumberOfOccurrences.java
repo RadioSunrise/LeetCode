c6f3615632923849750b33ec8b7d2fffb3c7a147
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-10-28 16:27:46 +0800

    Update UniqueNumberOfOccurrences.java

--- Solution/UniqueNumberOfOccurrences.java
+++ Solution/UniqueNumberOfOccurrences.java
@@ -1,7 +1,6 @@
 // site: https://leetcode-cn.com/problems/unique-number-of-occurrences/
 
 // 判断arr中每个数的出现次数是不是都不相同，用hasmap来记录出现次数，用hashset来检测重复
-// set判断重复也可以用全部加入set之后，判断set的大小是否和arr大小相同来给出是否重复
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
         Map<Integer, Integer> map = new HashMap<>();
@@ -23,5 +22,24 @@ class Solution {
             }
         }
         return true;
+    }
+}
+// set判断重复也可以用全部加入set之后，判断set的大小是否和map大小相同来给出是否重复，因为本身set的add函数就会判断重复
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int num : arr){
+            if(map.containsKey(num)){
+                map.put(num, map.get(num) + 1);
+            }
+            else {
+                map.put(num, 1);
+            }
+        }
+        Set<Integer> set = new HashSet<>();
+        for(int num : map.values()){
+            set.add(num);
+        }
+        return set.size() == map.size();
     }
 }
