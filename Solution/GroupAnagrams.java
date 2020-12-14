2a84c446f5219affeb33ec2a894295072bb2b73d
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-12-14 20:20:22 +0800

    Update GroupAnagrams.java

--- Solution/GroupAnagrams.java
+++ Solution/GroupAnagrams.java
@@ -34,3 +34,43 @@ public class GroupAnagrams {
         System.out.println(res.toString());
     }
 }
+
+// 2020-12-14
+// site: https://leetcode-cn.com/problems/group-anagrams/
+
+// 字母异位词分组，用的是排序之后相同串的方法，稍微会比较慢
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        // 异位词在排序完之后是完全一样的，可以用排序完的字符串作为key
+        // 用统计数组变成固定格式字符串也行，排序写起来方便一点
+        List<List<String>> res = new LinkedList<>();
+        Map<String, List<String>> map = new HashMap<>();
+        // 遍历
+        for(String str : strs){
+            // 排序之后加入map
+            char[] charArr = str.toCharArray();
+            Arrays.sort(charArr);
+            String sortStr = String.valueOf(charArr);
+            if(map.containsKey(sortStr)){
+                map.get(sortStr).add(str);
+            }
+            else {
+                List<String> list = new LinkedList<>();
+                list.add(str);
+                map.put(sortStr, list);
+            }
+        }
+        // 从map中构建答案
+        // 方法1，比较慢
+        /*
+        for(List<String> value : map.values()){
+            res.add(value);
+        }
+        return res;
+        */
+        
+        // 方法2，方便一些
+        return new ArrayList<List<String>>(map.values());
+    }
+}
