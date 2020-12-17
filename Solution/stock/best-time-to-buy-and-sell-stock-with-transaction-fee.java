ab9c05a89ef730cdcd4bfe1c6862fa4115943cb7
RadioSunrise <62924358+RadioSunrise@users.noreply.github.com>
2020-12-17 19:54:18 +0800

    Update best-time-to-buy-and-sell-stock-with-transaction-fee.java

--- Solution/stock/best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ Solution/stock/best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -21,3 +21,35 @@ class Solution {
         return dp_i_0;
     }
 }
+
+// 2020-12-17
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        int[][] dp = new int[n][2];
+        // base case
+        // dp[0][0] = 0, dp[0][1] = -price[0] (第0天已经持有股票)
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < n; i++){
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i] - fee);
+            dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+        }
+        // 最后一天手上没股票是最大的利润
+        return dp[n - 1][0];
+    }
+}
+class Solution {
+    public int maxProfit(int[] prices, int fee) {
+        int n = prices.length;
+        // 一维dp
+        int dp_0 = 0;
+        int dp_1 = -prices[0];
+        for(int i = 0; i < n; i++){
+            int temp = dp_0;
+            dp_0 = Math.max(dp_0, dp_1 + prices[i] - fee); 
+            dp_1 = Math.max(dp_1, temp - prices[i]);
+        }
+        return dp_0;
+    }
+}
